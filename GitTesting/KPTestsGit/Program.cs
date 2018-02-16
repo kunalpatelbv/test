4dd4ec322cc4bacedc3dadd4587a343040da0365
kunalpatelbv <kunal.patel@brandview.com>
2018-02-16 15:34:51 +0000

    test

---
+++ GitTesting/KPTestsGit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPTestsGit
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+
+            List<Task<int>> tasks = Enumerable.Range( 0, 1000).Select( i => new Task<int>( () => i * i ) ).ToList();
+            tasks.ForEach( t => t.Start() );
+            Task.WaitAll( tasks.ToArray() );
+            tasks.ForEach( t => Console.WriteLine( t.Result ) );
+            Console.WriteLine("Hello");
+        }
+    }
+}
